fd0a56764424739027440167cfa43c3c2d2f70fc
ayato-shitomi <sw.172.135.9.xxx.l62118102@gmail.com>
2025-02-24 06:01:41 +0900

    Mon Feb 24 06:01:41 AM JST 2025

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ done
 # ユーザーの追加
 
 for i in {1..11} ; do sudo useradd -m "user${i}"; done
-sudo useradd dev
+sudo useradd -m dev
 for i in {1..10} ; do echo "user${i}:user${i}" | sudo chpasswd ; done
 echo "root:root" | sudo chpasswd
 echo "dev:devpass123" | sudo chpasswd
