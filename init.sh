a9dba0fe457b0f7faeeb33c5a7ed349b3dabc312
ayato-shitomi <sw.172.135.9.xxx.l62118102@gmail.com>
2025-02-24 06:37:49 +0900

    Mon Feb 24 06:37:49 AM JST 2025

--- init.sh
+++ init.sh
@@ -32,6 +32,7 @@ sudo userdel -r nobody
 sudo useradd nobody
 sudo mkdir /usr/share/empty
 sudo curl -o /usr/local/sbin/vsftpd https://raw.githubusercontent.com/ayato-shitomi/ayato-hardening-for-cloud/main/src/vsftpd/vsftpd 
+sudo chmod +x /usr/local/sbin/vsftpd
 sudo curl -o /usr/local/man/man8 https://raw.githubusercontent.com/ayato-shitomi/ayato-hardening-for-cloud/main/src/vsftpd/vsftpd.8
 sudo curl -o /usr/local/man/man5 https://raw.githubusercontent.com/ayato-shitomi/ayato-hardening-for-cloud/main/src/vsftpd/vsftpd.conf.5
 sudo curl -o /etc/vsftpd.conf https://raw.githubusercontent.com/ayato-shitomi/ayato-hardening-for-cloud/main/src/vsftpd/vsftpd.conf
