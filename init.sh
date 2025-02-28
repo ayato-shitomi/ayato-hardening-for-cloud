f36fe9e1698e7dcd9c22f0fa2d06d91a92c0278e
ayato-shitomi <sw.172.135.9.xxx.l62118102@gmail.com>
2025-02-28 22:18:27 +0900

    Fri Feb 28 10:18:27 PM JST 2025

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@
 sudo apt update
 sudo apt install -y build-essential
 sudo apt install -y python3-pip
-pip3 install flask --break-system-packages
+pip3 install flask
 sudo apt install -y apache2
 
 # ユーザーの初期化
